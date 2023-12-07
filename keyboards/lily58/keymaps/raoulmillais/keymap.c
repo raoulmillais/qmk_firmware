56d2482e178f53d70ceaca676d1fceb8491d53e7
Raoul Millais <raoul@raoulmillais.com>
2023-12-07 12:41:09 +0000

    lily58: move delete onto number layer

--- keyboards/lily58/keymaps/raoulmillais/keymap.c
+++ keyboards/lily58/keymaps/raoulmillais/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT(
   KC_GRV,  KC_1,   KC_2,   KC_3,     KC_4,   KC_5,                      KC_6,   KC_7,  KC_8,     KC_9,    KC_0,     KC_BSPC,
-  KC_TAB,  KC_Q,   KC_W,   KC_E,     KC_R,   KC_T,                      KC_Y,   KC_U,  KC_I,     KC_O,    KC_P,     KC_DEL,
+  KC_TAB,  KC_Q,   KC_W,   KC_E,     KC_R,   KC_T,                      KC_Y,   KC_U,  KC_I,     KC_O,    KC_P,     KC_BSPC,
   KC_ESC,  KC_A,   KC_S,   KC_D,     KC_F,   KC_G,                      KC_H,   KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_QUOT,
   KC_LSFT, KC_Z,   KC_X,   KC_C,     KC_V,   KC_B,   KC_LBRC, KC_RBRC,  KC_N,   KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_RCTL,
                   KC_LCTL, KC_LALT,  SYM,    KC_SPC,                    KC_ENT, NUM,   KC_RGUI,  KC_RCTL),
@@ -74,8 +74,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Symbols */
 [_SYM] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-  _______, KC_EXLM,KC_AT,  KC_LCBR,  KC_RCBR, UC(0xA3),                   KC_AMPR, KC_LT,   KC_GT,    KC_EQL,  KC_ASTR, _______,
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______,  _______,
+  _______, KC_EXLM,KC_AT,  KC_LCBR,  KC_RCBR, UC(0xA3),                   KC_AMPR, KC_LT,   KC_GT,    KC_EQL,  KC_ASTR, KC_DEL,
   _______, KC_HASH,KC_DLR, KC_LPRN,  KC_RPRN, KC_GRV,                     KC_PIPE, KC_BSLS, KC_SLSH,  KC_PLUS, KC_MINS, _______,
   _______, KC_PERC,KC_CIRC,KC_LBRC,  KC_RBRC, KC_TILD,  _______, _______, KC_GRV,  KC_DQUO, KC_QUOT,  KC_UNDS, KC_TILD, _______,
                    _______,_______,  _______, _______,                    _______, _______,  _______,  _______),
